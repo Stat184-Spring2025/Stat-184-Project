150ad315c684a3033ad9dd66e2db81110eeeed85
Lucas Sadoulet <157857700+Lucas-Sadoulet-psu@users.noreply.github.com>
2025-04-27 22:58:04 -0400

    fix conversion error with commas

--- cleaning.r
+++ cleaning.r
@@ -1,26 +1,30 @@
 library(dplyr)
 library(tidyr)
-data <- read.csv("amazon.csv")
- 
- 
+data <- read.csv("C:/Users/llsad/Downloads/amazon.csv")
+
+
 View(data)
 df_unique <- data %>%
   distinct(product_id, .keep_all = TRUE)
- 
- 
+
+
 conversion_rate <- 0.012  # ₹1 = $0.012
- 
+
 df_usd <- df_unique %>%
   mutate(
-    PriceINR_clean = as.numeric(gsub("₹|,", "", actual_price)),
-    PriceUSD = PriceINR_clean * 0.012
+    actual_price_USD = as.numeric(gsub(",","",gsub("₹", "", actual_price))),
+    discounted_price_USD = as.numeric(gsub(",","",gsub("₹", "", discounted_price)))
+    
   )
- 
+View(df_usd)
+
+
 df_tidy <- df_usd %>%
   mutate(
     rating_count = as.numeric(gsub(",", "", rating_count))
+    
   ) %>%
   select(product_name, category, PriceUSD, rating, rating_count, user_id) %>%
   separate_rows(user_id, sep = ",")
- 
+
 View(df_tidy)
